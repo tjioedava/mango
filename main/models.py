dcbe9590b3e5ee93da5cd0bceeac1849fafdd8fd
tjioedava <tjioedava@gmail.com>
2024-09-08 21:07:46 +0700

    created product model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=127)
+    price = models.PositiveIntegerField()
+    description = models.TextField()
+    items_purchased = models.PositiveIntegerField(default=0)
+    rating = models.PositiveSmallIntegerField(default=0)
+
+    #derived attribute
+    @property
+    def recommended(self):
+        return self.rating > 4.5
