87f77f1b327423028568a9249586f5b4d319d1c1
tjioedava <tjioedava@gmail.com>
2024-09-15 20:09:13 +0700

    added functionality to request for product entry/entries data in form of XML or JSON by default

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, reverse
 from django.shortcuts import HttpResponseRedirect
-from .views import home, create_product
+from .views import *
 
 app_name = 'main'
 
 urlpatterns = [
     path('', lambda request: HttpResponseRedirect(reverse('main:home')), name='default'),
     path('home', home, name='home'),
-    path('create-product', create_product, name='create-product')
+    path('create-product', create_product, name='create-product'),
+    path('show-products', show_products, name='show-products'),
+    path('show-product-by-id/<str:pk>', show_product_by_id, name='show-product-by-id'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404
+from django.views.decorators.http import require_http_methods
+from django.core.serializers import serialize
 from .forms import ProductForm
 from .models import Product
 
@@ -20,3 +23,30 @@ def create_product(request):
     return render(request, 'create-product.html', {
         'form': form,
     })
+
+@require_http_methods(['GET',])
+def show_products(request):
+
+    format = request.GET.get('format', 'json')
+
+    if format not in ['json', 'xml']:
+        raise Http404()
+
+    products = Product.objects.all()
+    return HttpResponse(serialize(format, products), content_type=f'application/{format}')
+
+@require_http_methods(['GET',])
+def show_product_by_id(request, pk):
+
+    format = request.GET.get('format', 'json')
+
+    if format not in ['json', 'xml']:
+        raise Http404()
+    
+    products = Product.objects.filter(pk=pk)
+
+    if not products:
+        return HttpResponse(f'Product with id {pk} does not exist')
+
+    return HttpResponse(serialize(format, products), content_type=f'application/{format}')
+    
